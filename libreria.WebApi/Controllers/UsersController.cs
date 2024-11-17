835242b1f83b9e2977444a29a559e1e7636d9284
estiven2111 <estiven.a.martinez@gmail.com>
2024-11-17 16:52:36 -0500

    primer

---
+++ libreria.WebApi/Controllers/UsersController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace libreria.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        public UsersController()
+        {
+
+        }
+    }
+}
